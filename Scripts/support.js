83363493af22c2e7183c9e6b288dd0306a5ae810
A0l1i1c3e4 <vas120704@gmail.com>
2026-02-28 14:25:40 +0700

    Main Pages added

---
+++ Scripts/support.js
@@ -0,0 +1,48 @@
+const loginButton = document.getElementById('in');
+const profileButton = document.getElementById('profileButton');
+const logoutButton = document.getElementById('logoutButton');
+const userMenu = document.getElementById('userMenu');
+let userMenuListenerAttached = false;
+
+function activate(email){
+  if (!userMenuListenerAttached) {
+    loginButton.addEventListener('click', () => {
+    userMenu.style.display = userMenu.style.display === 'block' ? 'none' : 'block';
+    });
+    loginButton.textContent = email + ' ▾';
+    profileButton.style.display = 'inline-block';
+    logoutButton.style.display = 'inline-block';
+
+    profileButton.addEventListener('click', () => {
+      window.location.href = '../pages/profile.html'
+    });
+
+    logoutButton.addEventListener('click', () => {
+      fetch('http://localhost:8080/auth/logout', {  
+        method: 'DELETE',  
+        headers: {  
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        },  
+      })  
+      .catch(error => {  
+        console.error('Ошибка выхода из профиля:', error);  
+        alert('Ошибка выхода из профиля: ' + error.message);  
+      });  
+      localStorage.removeItem('token');
+      window.location.href = '../pages/login.html'
+    });
+    userMenuListenerAttached = true;
+  }
+}
+window.addEventListener('load', () => {
+    const authToken = localStorage.getItem('token');
+    if (authToken) {
+      console.log('Токен получен из localStorage:', localStorage.getItem('token'));
+      email=localStorage.getItem('email')
+      document.getElementById('in').textContent=email;
+      activate(email);
+    } else {
+      console.log('Токен не найден в localStorage.');
+      //window.location.href = '../pages/login.html'
+    }
+});
