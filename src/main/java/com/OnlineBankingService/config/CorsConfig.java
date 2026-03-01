b582b944b88c125cba1575dedebbc798484e3978
Mikhail Nosov <misha.d.nosov@gmail.com>
2026-03-01 20:38:15 +0700

    cors fix

--- src/main/java/com/OnlineBankingService/config/CorsConfig.java
+++ src/main/java/com/OnlineBankingService/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5500", "http://localhost:8084", "http://localhost:8085", "http://localhost:8081", "http://127.0.0.1:5500")
+                        .allowedOrigins("http://localhost:5500", "http://localhost:8084", "http://localhost:8085", "http://localhost:8081", "http://127.0.0.1:5500", "http://127.0.0.1:5501")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
