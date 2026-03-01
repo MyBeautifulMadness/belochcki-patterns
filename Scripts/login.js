acc7087c5d7c00773e8ed6c2718cb99c610c2e74
Valentin Voronin <vallvoron@yandex.ru>
2026-03-01 14:48:53 +0700

    login and users lock/unlock pages

---
+++ Scripts/login.js
@@ -0,0 +1,71 @@
+window.addEventListener('load', async () => {
+  const token = localStorage.getItem('token');
+
+  if (!token) return;
+
+  try {
+    const res = await fetch('http://localhost:8085/api/auth/validate', {
+      method: 'POST',
+      headers: {
+        'Authorization': token
+      }
+    });
+
+    if (res.ok) {
+      console.log('Токен валиден, редиректим на main.html');
+      window.location.href = '../pages/main.html';
+    } else {
+      console.log('Токен невалиден');
+      localStorage.clear();
+    }
+  } catch (e) {
+    console.error('Ошибка при проверке токена', e);
+    localStorage.clear();
+  }
+});
+
+
+function a(){ 
+  const email = document.getElementById('email').value; 
+  const password = document.getElementById('password').value; 
+
+  if (email === '' || password === '') { 
+    alert('Пожалуйста, заполните все поля!'); 
+    return; 
+  } 
+  else{
+    fetch('http://localhost:8085/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        login: email,
+        password: password,
+        userType: "EMPLOYEE"
+      })
+    })
+    .then(response => { 
+      if (!response.ok) { 
+        
+        return response.text().then(text => { throw new Error(text) }); 
+      } 
+      return response.json();
+    }) 
+    .then(data => { 
+      const token = data.token; 
+
+      if (token) {  
+        localStorage.setItem('token', token);
+        localStorage.setItem('email', email);
+        window.location.href = '../Pages/main.html'
+      } else {  
+        console.error('Токен не найден в заголовке ответа.');  
+      }  
+    }) 
+    .catch(error => { 
+      console.error('Ошибка входа:', error); 
+      alert('Ошибка входа: ' + error.message); 
+    });
+  }
+}
---
+++ Scripts/profile.js
@@ -0,0 +1,152 @@
+const username = document.getElementById('username');
+const birthdate = document.getElementById('birthdate');
+const useremail = document.getElementById('email');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const phone = document.getElementById('phone');
+
+const savebutton = document.getElementById('save');
+const loginButton = document.getElementById('in');
+const profileButton = document.getElementById('profileButton');
+const logoutButton = document.getElementById('logoutButton');
+const userMenu = document.getElementById('userMenu');
+let userMenuListenerAttached = false;
+
+function activate(email){
+  if (!userMenuListenerAttached) {
+    loginButton.addEventListener('click', () => {
+    userMenu.style.display = userMenu.style.display === 'block' ? 'none' : 'block';
+    });
+    loginButton.textContent = email + ' ▾';
+    profileButton.style.display = 'inline-block';
+    logoutButton.style.display = 'inline-block';
+
+    profileButton.addEventListener('click', () => {
+      window.location.href = '../pages/profile.html'
+    });
+
+    logoutButton.addEventListener('click', () => {
+      fetch('http://localhost:8080/auth/logout', {  
+        method: 'DELETE',  
+        headers: {  
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        },  
+      })  
+      .catch(error => {  
+        console.error('Ошибка выхода из профиля:', error);  
+        alert('Ошибка выхода из профиля: ' + error.message);  
+      });  
+      localStorage.removeItem('token');
+      window.location.href = '../pages/login.html'
+    });
+    userMenuListenerAttached = true;
+  }
+}
+
+function formatDate(dateString) { 
+  const date = new Date(dateString); 
+  const year = date.getFullYear(); 
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0'); 
+ 
+  return `${year}-${month}-${day}`; 
+} 
+ 
+window.addEventListener('load', () => { 
+  const authToken = localStorage.getItem('token'); 
+  if (authToken) { 
+    email=localStorage.getItem('email') 
+    document.getElementById('in').textContent=email; 
+    activate(email); 
+    fetch('http://localhost:8080/auth/profile', {  
+      method: 'GET',  
+      headers: {  
+        Authorization: `Bearer ${localStorage.getItem('token')}`, 
+        'Content-Type': 'application/json'  
+      },  
+    })  
+    .then(response => {  
+      if (!response.ok) {  
+          
+        return response.text().then(text => { throw new Error(text) });  
+      }  
+      return response.json(); 
+    }) 
+    .then(data => { 
+      username.value = data.username || '';
+      birthdate.value = formatDate(data.birthday) || '';
+      useremail.value = data.email || '';
+      firstName.value = data.firstName || '';
+      lastName.value = data.lastName || '';
+      phone.value = data.phone || '';
+      console.log(data.phone);
+      savebutton.addEventListener('click',()=>{ 
+        fetch('http://localhost:8080/auth/profile', {  
+          method: 'PUT',  
+          headers: {  
+            Authorization: `Bearer ${localStorage.getItem('token')}`, 
+            'Content-Type': 'application/json' 
+          },  
+          body: JSON.stringify({ birthday: birthdate.value, email: useremail.value, firstName: firstName.value, lastName: lastName.value, phone: phone.value})  
+        })  
+        .then(response => {  
+          
+          if (!response.ok) {  
+            return response.text().then(text => { throw new Error(text) });  
+          } 
+        })  
+      }); 
+    })  
+    .catch(error => {  
+      console.error('Ошибка получения параметров профиля:', error);  
+      alert('Ошибка получения параметров профиля: ' + error.message);  
+    });  
+  } else { 
+    console.log('Токен не найден в localStorage.'); 
+    loginButton.addEventListener('click', () => { 
+      window.location.href = '../pages/login.html'; 
+    }); 
+  } 
+}); 
+$(function() {  
+    $( "#birthdate" ).datepicker({  
+      dateFormat: "yy-mm-dd", 
+      changeMonth: true,  
+    changeYear: true,  
+      yearRange: "1900:2024", 
+    });  
+  }
+);  
+ 
+$(function() {  
+  $("#phone").keyup(function() {  
+    let phone = $(this).val().replace(/\D/g, "");  
+    let formattedPhone = "";  
+    phone = phone.substring(0, 12); 
+      
+    if (phone.length > 0) {  
+      formattedPhone += "+7 (";  
+    } 
+    if (phone.length >= 4) {  
+      formattedPhone += phone.substring(1, 4) + ") ";  
+    } else {  
+      formattedPhone += phone.substring(1, phone.length);  
+    }  
+    if (phone.length >= 7) {  
+      formattedPhone += phone.substring(4, 7) + "-";  
+    } else if (phone.length > 4) {  
+        formattedPhone += phone.substring(4, phone.length);  
+    }  
+    if (phone.length >= 9) {  
+      formattedPhone += phone.substring(7, 9) + "-";  
+    } else if (phone.length > 7) {
+      formattedPhone += phone.substring(7, phone.length) ;  
+    }  
+    if (phone.length >= 11) {  
+      formattedPhone += phone.substring(9, 11);  
+    } else if (phone.length > 9) {  
+        formattedPhone += phone.substring(9, phone.length);  
+    }  
+    $(this).val(formattedPhone);  
+  });  
+}); 
