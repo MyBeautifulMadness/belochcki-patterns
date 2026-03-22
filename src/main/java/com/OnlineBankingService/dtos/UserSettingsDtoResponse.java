8ea9da8f508f4782b27efd5cefa7fcec8151408b
Valentin Voronin <vallvoron@yandex.ru>
2026-03-22 16:16:14 +0700

    fix in creating user

--- src/main/java/com/OnlineBankingService/dtos/UserSettingsDtoResponse.java
+++ src/main/java/com/OnlineBankingService/dtos/UserSettingsDtoResponse.java
@@ -1,7 +1,23 @@
 package com.OnlineBankingService.dtos;
 
+import com.OnlineBankingService.entities.Theme;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserSettingsDtoResponse {
+
+    private UUID userId;
+
+    private Theme theme;
+
+    private List<UUID> hiddenAccountIds;
 }
