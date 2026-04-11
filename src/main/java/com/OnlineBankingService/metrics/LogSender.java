300beacb93a5d906e5756f2dfa90ee3fa1b07df9
Mikhail Nosov <misha.d.nosov@gmail.com>
2026-04-12 00:22:46 +0700

    +fix errorRate send

--- src/main/java/com/OnlineBankingService/metrics/LogSender.java
+++ src/main/java/com/OnlineBankingService/metrics/LogSender.java
@@ -4,6 +4,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 public class LogSender {
 
@@ -21,6 +22,36 @@ public class LogSender {
                         "{\"service\":\"%s\",\"message\":\"%s\"}",
                         service,
                         message.replace("\"", "'")
+                );
+
+                try (OutputStream os = conn.getOutputStream()) {
+                    os.write(body.getBytes(StandardCharsets.UTF_8));
+                }
+
+                conn.getResponseCode();
+
+            } catch (Exception ignored) {
+            }
+        }).start();
+    }
+
+    public static void sendMinuteMetrics(String service, double errorRate) {
+        new Thread(() -> {
+            try {
+                URL url = new URL("http://localhost:8090/logs/errorRate");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setDoOutput(true);
+
+                String time = LocalDateTime.now().toString();
+
+                String body = String.format(
+                        "{\"service\":\"%s\",\"message\":\"errorRate=%.2f%% time=%s\"}",
+                        service,
+                        errorRate,
+                        time
                 );
 
                 try (OutputStream os = conn.getOutputStream()) {
